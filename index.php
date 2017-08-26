ad880483ceb7c9156a457dfde0d6a4c70fa6a0db
cvium <clausvium@gmail.com>
2017-08-26 14:56:20 +0200

    moved script tags

--- index.php
+++ index.php
@@ -80,6 +80,9 @@
 
     <link href="css/<?php echo DEFAULT_THEME; ?>.bootstrap.min.css" rel="stylesheet">
     <link href="css/hack.css" rel="stylesheet">
+    <script src="js/jquery.min.js" defer></script>
+    <script src="js/bootstrap.min.js" defer></script>
+    <script src="js/live.js" defer></script>
     </head>
     <body>
         <div class="container">
@@ -121,7 +124,6 @@
                                          '</div>',
                                          '<div id="upload">Upload: FETCHING',
                                          '</div>',
-                                         '<script src="js/live.js"></script>',
                                          '<script type="text/javascript">',
                                          '$(document).ready(startPolling("' . $iface . '", '. LIVE_DATA_INTERVAL .'));',
                                          '</script>'
@@ -196,7 +198,5 @@
                 </div>
             </div>
         </div>
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
